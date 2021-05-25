976d2b957191800477211a3e20e521c76889f4f4
fruityvegetables <gg.troyer@gmail.com>
2021-05-25 16:34:08 -0500

    found an error pt. 6

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ function sendMail() {
     message: document.getElementById("message").value,
   };
 
-  emailjs.send("service_qzqbnkr", "template_yz9hi99").then(function (res) {
-    console.log("success", res.status);
-  });
+  emailjs
+    .send("service_qzqbnkr", "template_yz9hi99", tempParams)
+    .then(function (res) {
+      console.log("success", res.status);
+    });
 }
